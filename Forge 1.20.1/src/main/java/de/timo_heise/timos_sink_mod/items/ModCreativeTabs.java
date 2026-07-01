e2e74e6f39f36ad1ea5a037d656c237309cba25c
Comedywaran <m1n3cr4f7undr0bl0x@gmail.com>
2026-07-01 09:54:07 +0200

    added placeholder texture & lang file

--- Forge 1.20.1/src/main/java/de/timo_heise/timos_sink_mod/items/ModCreativeTabs.java
+++ Forge 1.20.1/src/main/java/de/timo_heise/timos_sink_mod/items/ModCreativeTabs.java
@@ -3,6 +3,7 @@ package de.timo_heise.timos_sink_mod.items;
 import de.timo_heise.timos_sink_mod.TimosSinkMod;
 import de.timo_heise.timos_sink_mod.blocks.ModBlocks;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,6 +16,7 @@ public class ModCreativeTabs {
     public static final RegistryObject<CreativeModeTab> SINK_TAB = CREATIVE_MODE_TABS.register("timos_sink_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> ModBlocks.SINK_BLOCK.get().asItem().getDefaultInstance())
+            .title(Component.translatable("itemGroup.timos_sink_tab"))
             .displayItems((parameters, output) -> {
                 output.accept(ModBlocks.SINK_BLOCK.get());
             }).build());

d89f09f3e50f802f63afdad5629ba60341b8af31
Comedywaran <m1n3cr4f7undr0bl0x@gmail.com>
2026-09-12 00:39:08 +0200

    made it pretty

--- Forge 1.20.1/src/main/java/de/timo_heise/timos_sink_mod/menus/widgets/ScrollPanel/SinkScrollPanel.java
+++ Forge 1.20.1/src/main/java/de/timo_heise/timos_sink_mod/menus/widgets/ScrollPanel/SinkScrollPanel.java
@@ -1,30 +1,42 @@
 package de.timo_heise.timos_sink_mod.menus.widgets.ScrollPanel;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.logging.LogUtils;
+import de.timo_heise.timos_sink_mod.TimosSinkMod;
 import de.timo_heise.timos_sink_mod.menus.ClientUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.gui.widget.ScrollPanel;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 /**
  * code inspired by {@link net.minecraftforge.client.gui.ModListScreen.InfoPanel}
- * <br>TODO: widgets, fancy graphics, mouse hovering outside of panel, getContentHeight(), getScrollAmount()?, comments
+ * <br>TODO: widgets, mouse hovering outside of panel, getContentHeight(), getScrollAmount()?, comments
  */
 public class SinkScrollPanel extends ScrollPanel {
     private final Set<AbstractWidget> widgets = new LinkedHashSet<>();
     private final Set<Renderable> renderables = new LinkedHashSet<>();
     private int yOffset;
     private static final int BORDER = 4;
+    private static final int SPACEBETWEENPANELANDBAR = 3;
     private static final int BARWIDTH = 6;
+    private static final ResourceLocation resourceLocation = ResourceLocation.fromNamespaceAndPath(TimosSinkMod.MOD_ID, "textures/gui/sink/sink.png");
 
-    public SinkScrollPanel(Minecraft mc, int x, int y, int width, int height)
+    public SinkScrollPanel(Minecraft mc, int x, int y, int width, int height) {
+        this(mc, x, y, width, height, getBGColor(), 0xFF8B8B8B, 0xFF555555, 0xFFC6C6C6);
+    }
+
+    private SinkScrollPanel(Minecraft mc, int x, int y, int width, int height, int bgColor, int barBgColor, int barColor, int barBorderColor)
     {
-        super(mc, width, height, y, x, BORDER, BARWIDTH);
+        super(mc, width-2, height-2, y+1, x+1, BORDER, BARWIDTH, bgColor, bgColor, barBgColor, barColor, barBorderColor);
     }
 
     @Override
@@ -45,7 +57,7 @@ public class SinkScrollPanel extends ScrollPanel {
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         super.render(guiGraphics, mouseX, mouseY, partialTick);
 
-        if (ClientUtil.betterIsHovering(left, top, width-BARWIDTH, height, mouseX, mouseY)) {
+        if (ClientUtil.betterIsHovering(left, top, width-SPACEBETWEENPANELANDBAR-BARWIDTH, height, mouseX, mouseY)) {
             guiGraphics.pose().pushPose();
             guiGraphics.pose().translate(0, yOffset, 0);
             for(Renderable renderable : renderables) {
@@ -55,6 +67,34 @@ public class SinkScrollPanel extends ScrollPanel {
             }
             guiGraphics.pose().popPose();
         }
+        drawFrame(guiGraphics);
+    }
+
+    protected void drawFrame(GuiGraphics guiGraphics) {
+
+        int panelRight = right-SPACEBETWEENPANELANDBAR-BARWIDTH;
+        for(int i = 1; i <= width-SPACEBETWEENPANELANDBAR-BARWIDTH-2; i++) {
+            // top edge:
+            guiGraphics.blit(resourceLocation, left+i, top-1, 23, 168, 1, 2);
+            // bottom edge:
+            guiGraphics.blit(resourceLocation, left+i, top+height-1, 23, 173, 1, 2);
+        }
+        for(int i = 1; i <= height-2; i++) {
+            // left edge:
+            guiGraphics.blit(resourceLocation, left-1, top+i, 20, 171, 2, 1);
+            // right edge:
+            guiGraphics.blit(resourceLocation, panelRight-1, top+i, 25, 171, 11, 1);
+        }
+        
+        // corners:
+        // top left:
+        guiGraphics.blit(resourceLocation, left-1, top-1, 20, 168, 2, 2);
+        // top right:
+        guiGraphics.blit(resourceLocation, panelRight-1, top-1, 25, 168, 11, 2);
+        // bottom left:
+        guiGraphics.blit(resourceLocation, left-1, bottom-1, 20, 173, 2, 2);
+        // bottom right:
+        guiGraphics.blit(resourceLocation, panelRight-1, bottom-1, 25, 173, 11, 2);
     }
 
     @Override
@@ -101,5 +141,17 @@ public class SinkScrollPanel extends ScrollPanel {
     public void addRenderableOnly(Renderable renderable) {
         renderables.add(renderable);
         //contentHeight = Math.max(contentHeight, bottomPos);
+    }
+
+    private static int getBGColor() {
+        try (InputStream stream = Minecraft.getInstance().getResourceManager().getResource(resourceLocation).orElseThrow(() -> new IOException("Texture not found")).open();
+             NativeImage image = NativeImage.read(stream)) {
+
+            int abgr = image.getPixelRGBA(23, 171); // don't know why it's called getPixelRGBA() if it returns abgr, but ok I guess
+            return (abgr & 0xFF00FF00) | ((abgr & 0x00FF0000) >>> 16) | ((abgr & 0x000000FF) << 16);
+        } catch (IOException e) {
+            LogUtils.getLogger().warn("Could not load texture \"{}\" for rendering SinkScrollPanel {}", resourceLocation, e);
+            return 0x00000000;
+        }
     }
 }
